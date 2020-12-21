19836bc129ac1af5f550b3c570a17bea09f92ee8
“zouweidong” <zouweidong@sendbp.com>
2020-12-21 16:47:45 +0800

    函数指针

--- c/chapter5/strstr.c
+++ c/chapter5/strstr.c
@@ -4,6 +4,13 @@
 
 排序函数：
     在指定可选参数-n的情况下，该函数将按照数值大小而非字典顺序对输入文本进行排序
+
+int (*comp)(void *, void *)
+comp是一个指向函数的指针，该函数具有两个void类型的参数, 其返回值为int类型
+
+if ((*comp)(v[i], v[left] < 0))
+comp使用和声明是一致的，*comp代表一个函数。
+(*comp)(v[i], v[left] < 0)  其中的圆括号是必须的
 */
 
 # include <stdio.h>
